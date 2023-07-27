10f4fec8c7c294d1cb4dbf49793267b742971939
Ekam Pandher <ekam@Ekams-MacBook-Pro.local>
2023-07-27 12:43:00 +0800

    Update http statuscodes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -54,6 +54,11 @@ func handleBuyerLogin(c *gin.Context) {
 
 	loginResponse, err := buyer.BuyerLogin(db, loginData)
 
+	if err != nil && err.Error() == "Something went wrong" {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
 	if err != nil {
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -124,6 +129,11 @@ func handleBuyerSignUp(c *gin.Context) {
 	}
 
 	signUpResponse, err := buyer.BuyerSignUp(db, signUpData)
+
+	if err != nil && err.Error() == "Something went wrong" {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
 
 	if err != nil {
 		c.JSON(err.ErrorCode(), gin.H{"message": err.Error()})
